b680f7c047d58f1bb0850f2ae2ef71612ca3d4a3
Erik Forkalsrud <erik.forkalsrud@impact.com>
2021-04-26 11:36:35 -0700

    Initial checkin

---
+++ src/main/groovy/com/impact/gradle/detangler/plugin/DetanglerPlugin.groovy
@@ -0,0 +1,69 @@
+package com.impact.gradle.detangler.plugin
+
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+import org.gradle.api.plugins.*
+
+
+class DetanglerPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.pluginManager.apply(ApplicationPlugin)
+        project.pluginManager.apply(JavaPlugin)
+
+        project.configurations.create("detanglerConfig")
+        project.dependencies.add("detanglerConfig", "com.seanshubin.detangler:detangler-console:0.9.4")
+
+        DetanglerPluginExtension spec = project.extensions.create('detangler', DetanglerPluginExtension)
+
+        project.tasks.create("detangler", JavaExec) {
+            main 'com.seanshubin.detangler.console.ConsoleApplication'
+            classpath project.configurations.detanglerConfig
+            dependsOn 'compileJava'
+            dependsOn 'compileTestJava'
+
+            File allowedFile
+            File argsFile
+
+            doFirst {
+                def startsWith = spec.basePackages.collect {"[" + it.replace('.', ' ') + "]"}.join(" ")
+
+                allowedFile = File.createTempFile("detangler-allowed-in-cycle", ".txt")
+                allowedFile.write(spec.allowedInCycle.collect {"[" + it.replace('.', ' ') + "]"}.join(" "))
+
+                String classesDir = project.sourceSets.main.output.classesDirs.join(" ")
+                String testClassesDir = project.sourceSets.test.output.classesDirs.join(" ")
+
+                argsFile = File.createTempFile("detangler", ".txt")
+                argsFile.write(String.join(
+                        "\n",
+                        "{",
+                        "  reportDir build/report/detangler",
+                        "  searchPaths [ " + classesDir + " " + testClassesDir + " ]",
+                        "  level 2",
+                        "  startsWith {",
+                        "    include [" + startsWith + "]",
+                        "    drop [" + startsWith + "]",
+                        "    exclude []",
+                        "  }",
+                        "  ignoreFiles []",
+                        "  canFailBuild true",
+                        "  ignoreJavadoc true",
+                        "  logTiming false",
+                        "  logEffectiveConfiguration false",
+                        "  allowedInCycle " + allowedFile.getAbsolutePath(),
+                        "  pathsRelativeToCurrentDirectory true",
+                        "  pathsRelativeToConfigurationDirectory false",
+                        "}"))
+
+                args(argsFile.path)
+            }
+
+            doLast {
+                allowedFile.delete()
+                argsFile.delete()
+            }
+        }
+    }
+}
+
---
+++ src/main/groovy/com/impact/gradle/detangler/plugin/DetanglerPluginExtension.groovy
@@ -0,0 +1,9 @@
+package com.impact.gradle.detangler.plugin
+
+class DetanglerPluginExtension {
+
+    def basePackages
+    def allowedInCycle
+
+}
+
